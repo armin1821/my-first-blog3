10a8586517461804c9af0d8efeb4261e29f0df96
armin1821 <armin>
2021-08-20 09:31:41 +0430

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+    #return HttpResponse('hello, this is blog page.')
 
 # Create your views here.
---
+++ tarix/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.utils import timezone
+from django.http import HttpResponse
+def emrooz(request):
+    now=timezone.now()
+    return HttpResponse(str(now))
+
